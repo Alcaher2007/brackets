be3e8fe60fc08205d235480032b17c59e9fa3da2
Alcaher2007 <pilat.egor@gmail.com>
2019-03-20 11:51:02 +0300

    Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var hek=str.split('');
+let r=25;
+while(0<r)
+{
+r--;
+for(var i=0;i<bracketsConfig.length;i++)
+{
+for(var n=0;n<str.length;n++) 
+{
+if(hek[n]==bracketsConfig[i][0] && hek[n+1]==bracketsConfig[i][1])
+{
+hek.splice(n, 2);
+n=n-1;
+}
+else
+{
+continue;
+}
+}
+}
+}
+if(hek.length==0)
+{
+return true;
+}
+else
+{
+return false;
+}
+
 }
